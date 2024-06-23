68529ba9669c1a31a6527a6bc61442621c7dd60b
SarahBiscuit <150682385+SarahBiscuit@users.noreply.github.com>
2024-06-23 14:24:01 +0100

    Slight amendment to Javascript function
    Slight amendment to Javascript function

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 //ADD IN EVENT LISTENERS TO GET ACCORDION BUTTONS ON HOME PAGE TO WORK, AND GET ELEMENT BY IDS//
 //JAVASCRIPT CODE TO TOGGLE FIRST ACCORDION ON AND OFF//
 
-function toggleAccordion() {
-    const myCollapse = document.getElementById('collapseOne');
-    const bsCollapse = new bootstrap.Collapse(myCollapse, { toggle: true });
-}
+const firstHomepageAccordionItem = document.getElementById("collapseOne");
 
-toggleAccordion();
+//write toggleAccordion() function here.//
+
+firstHomepageAccordionItem.addEventListener("click", toggleAccordion());
 
 //CURRENTLY THIS CODE ISN'T WORKING AND I'M NOT SURE WHY//
