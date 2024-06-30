e4fcac1cdf3d93265c548eb4ef5c6b7bba403aec
SarahBiscuit <150682385+SarahBiscuit@users.noreply.github.com>
2024-06-30 17:26:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 //ADD IN EVENT LISTENERS TO GET ACCORDION BUTTONS ON HOME PAGE TO WORK, AND GET ELEMENT BY IDS//
 //JAVASCRIPT CODE TO TOGGLE FIRST ACCORDION ON AND OFF//
 
-const firstHomepageAccordionItem = document.getElementById("collapseOne");
+//Have added onClick="toggle(event)" to my buttons in my accordion.  Need to now write a 'toggle' function to work to toggle them on and off.//
 
-//write toggleAccordion() function here.//
-
-firstHomepageAccordionItem.addEventListener("click", toggleAccordion());
-
-//CURRENTLY THIS CODE ISN'T WORKING AND I'M NOT SURE WHY//
+//CURRENTLY THIS CODE ISN'T WORKING AND I'M NOT SURE WHY/
