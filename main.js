3e0db810f65861812cf91558437428a946dfa8f9
SarahBiscuit <150682385+SarahBiscuit@users.noreply.github.com>
2024-06-30 21:22:04 +0100

    HTML accordion formatting amended according to the guidance, but the Javascript toggle function is still not working on the accordion.

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-//ADD IN EVENT LISTENERS TO GET ACCORDION BUTTONS ON HOME PAGE TO WORK, AND GET ELEMENT BY IDS//
-//JAVASCRIPT CODE TO TOGGLE FIRST ACCORDION ON AND OFF//
+var homepageAccordion = new bootstrap.Collapse(document.getElementById('homepageAccordion'));
 
-//Have added onClick="toggle(event)" to my buttons in my accordion.  Need to now write a 'toggle' function to work to toggle them on and off.//
-
-//CURRENTLY THIS CODE ISN'T WORKING AND I'M NOT SURE WHY/
+homepageAccordion.show();
+homepageAccordion.hide();
