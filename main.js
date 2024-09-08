7d0e7fba9a4fd24f0e084f17f69c4a8299bce590
SarahBiscuit <150682385+SarahBiscuit@users.noreply.github.com>
2024-09-08 15:37:11 +0100

    Second accordion item

--- main.js
+++ main.js
@@ -1,29 +1,8 @@
 //FORM - ADD BORDERS TO THE FORM BOXES.  COLOUR THE SUBMIT BUTTON.  BUILD THE JAVASCRIPT FOR THE FORM (get the form entries to go to email address).//
-//The following are links showing info on how to do this.
 
-Form that sends you with the form input data: -
-
-https://blog.getform.io/how-to-create-an-html-form-that-sends-you-an-email/#:~:text=To%20enable%20sending%20data%20in%20the%20email%2C%20we,subject%20line%22%2C%20%24_POST%5B%22Insert%20Your%20Message%22%5D.%20%22From%3A%20jane%40janedoe.com%22%29%3B%20%7D
-https://stackoverflow.com/questions/60573053/how-to-make-an-html-form-send-data-to-an-email-address
-https://html.form.guide/email-form/html-email-form/
-https://tutorialdeep.com/create-forms/php-code-for-contact-form-send-email/
-https://formcarry.com/blog/how-to-create-a-simple-html-contact-form/
-https://www.codeease.net/programming/javascript/how-to-send-form-data-to-email-using-javascript
-https://blog.hubspot.com/marketing/html-form-email
-https://www.jotform.com/blog/how-to-create-a-form-in-html-and-send-it-to-email/
-
-Other ways of doing it that are simple for the end user: -
-https://davidkehinde.hashnode.dev/form-processing-with-javascript
-https://www.freecodecamp.org/news/how-to-submit-a-form-with-javascript/
-https://mailtrap.io/blog/javascript-contact-form/
-https://www.shecodes.io/athena/3609-how-to-submit-a-form-and-send-data-to-an-api-using-javascript
-https://www.freecodecamp.org/news/form-validation-in-javascript/
-https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit
-https://www.tutorialspoint.com/how-to-submit-an-html-form-using-javascript
-https://www.reddit.com/r/AskProgramming/comments/14qdp5r/create_a_web_form_to_collect_information_from/
-//
-
-
-
+//R.e. ENQUIRIES FORM, hAVE GOT SOME ADVICE ONLINE AND I THINK THAT IT MIGHT BE MORE USEABLE ON ELIZABETH'S END TO LINK TO MICROSOFT FORMS OR GOOGLE FORMS (Need to check if she can access these for free).  
+THE OTHER OPTION IS TO FIND EXAMPLES OF PHP CODE ONLINE FOR SIMPLE ENQUIRY FORM AND CREATE VERSION OF THEM.//
+//HAVE FOUND PHP TUTORIAL VIDEO ON https://www.bing.com/videos/riverview/relatedvideo?q=php+cod+for+email+contact+form&mid=C27C8A10F2527262B6D5C27C8A10F2527262B6D5&FORM=VIRE
+WOULD HAVE TO MAKE FORM FAR MORE SIMPLE. BUT THIS SHOULD WORK.//
 
 //ADD ELIZBETH COPYRIGH STATEMENT (OR LINK TO IT) IN FOOTER BAR.//
