a25f72456ef88e4419f26f8a46d7a120339c99d2
SarahBiscuit <150682385+SarahBiscuit@users.noreply.github.com>
2024-09-11 20:25:30 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -4,5 +4,10 @@ $name = $_POST["Name"];
 $email = $_POST["emailAddress"];
 $message = $_POST["message"];
 
+$mailheader = "From:".$name."<".$email.">\r\n"
+
+$myemail = "scfletcher2000@hotmail.com";
+
+mail
 
 ?>
