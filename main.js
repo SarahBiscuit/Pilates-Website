8312ee6c9adea839880ca2c0be7f2f6e21a05f9f
SarahBiscuit <150682385+SarahBiscuit@users.noreply.github.com>
2024-12-07 21:38:25 +0000

    Fixed issue with border appearing when click on accordinon buttons

--- main.js
+++ main.js
@@ -12,3 +12,4 @@ console.log(validateEmail(email));
 
 //This code currenlty logs true or false in the console, but need it to prevent the user from submitting the form if it is not a valid email address.  
 //Could also get it to bring up an error message (e.g. an alert) for the user if they enter an invalid email address.
+//Want to use jQuery event listener on submit button
