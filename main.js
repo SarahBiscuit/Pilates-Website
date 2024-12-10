a981dbf027af7e37f6c3367444d0b83ff2a46224
SarahBiscuit <150682385+SarahBiscuit@users.noreply.github.com>
2024-12-10 22:06:27 +0000

    Update main.js

--- main.js
+++ main.js
@@ -4,23 +4,22 @@ let emailAddress = document.getElementById("emailAddress");
 
 //regex function to check if is valid email
 
-function validateEmail (email) {
+$(document).ready(function() {
+function validateEmail(email) {
 const emailValidationRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 return emailValidationRegex.test(email);
 }
-
-//running the function
-
-const email = emailAddress.value;
-console.log(validateEmail(email));
 
 //jQuery eventListener to check if email is valid, and if it is not to bring up alert message and make sure the form does not submit.
 
 //can't figure out why my HTML file and my jquery file are not linking up
 
-$("#submitButton").submit(function(event){
-    if (validateEmail(email) === false) {
+$("#enquiriesForm").submit(function(event){
+    const email = $("#emailAddress").val();
+    if (!validateEmail(email)) {
         alert("Invalid email - please amend email address");
         event.preventDefault();
-})
+}
+});
+});
 
