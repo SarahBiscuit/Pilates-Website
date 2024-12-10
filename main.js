959174453d849c7fd3566ead5fe5268bd4d214c2
SarahBiscuit <150682385+SarahBiscuit@users.noreply.github.com>
2024-12-10 21:44:55 +0000

    jQuery should now be linking up to html files

--- main.js
+++ main.js
@@ -24,6 +24,3 @@ $("#submitButton").submit(function(event){
         event.preventDefault();
 })
 
-//This code currenlty logs true or false in the console, but need it to prevent the user from submitting the form if it is not a valid email address.  
-//Could also get it to bring up an error message (e.g. an alert) for the user if they enter an invalid email address.
-//Want to use jQuery event listener on submit button
